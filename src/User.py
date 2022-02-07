93d5abe7b97dc06715802de210fe3f793b7cb72f
barg991 <barg991@gmail.com>
2022-02-07 20:24:04 +0200

    added user object

---
+++ src/User.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self):
+        self.username = ''
+        self.address = ''
+
+    def set_user(self,username,adress):
+        self.username=username
+        self.address=adress
+    
