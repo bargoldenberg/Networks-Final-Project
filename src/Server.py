71dced2514ddebc9ee2281597d3cc954c83baf3f
barg991 <barg991@gmail.com>
2022-02-07 21:38:44 +0200

    started implementing server for chat

--- src/Server.py
+++ src/Server.py
@@ -1,18 +1,34 @@
-#import socket
+# import socket
 from socket import *
+from User import *
 
 SERVER_ADDRESS = ('', 55001)
 serverSocket = socket(AF_INET, SOCK_STREAM)
-
+connections = {}
+addresses = []
 serverSocket.bind(SERVER_ADDRESS)
 serverSocket.listen(5)
-print("The server is ready to receive client")
+print("The server is ready to receive clients")
 while True:
     connectionSocket, addrClient = serverSocket.accept()
-    sentence = connectionSocket.recv(4096).decode()
-
-    print("Get from client ",addrClient, ":", sentence)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(bytes(capitalizedSentence.encode()))
-    #connectionSocket.send(capitalizedSentence).encode()
-    connectionSocket.close()
+    # First connection.
+    if addrClient not in addresses:
+        addresses.append(addrClient)
+        user = User()
+        user.set_user(connectionSocket.recv(4096).decode(), addrClient)
+        connections[connectionSocket] = user
+    # Find to who the message is for and send
+    else:
+        sentence = connectionSocket.recv(4096).decode()
+        connected_user = connections[connectionSocket].connected_user
+        keys = connections.keys()
+        sendsock = None
+        for key in keys:
+            if connections[key] == connected_user:
+                sendsock = key
+        if sendsock is None:
+            print("No such user")
+        print("Get from client ", addrClient, ":", sentence)
+        sendsock.send(bytes(sentence.encode()))
+        # connectionSocket.send(capitalizedSentence).encode()
+        connectionSocket.close()
--- src/User.py
+++ src/User.py
@@ -2,8 +2,10 @@ class User:
     def __init__(self):
         self.username = ''
         self.address = ''
-
+        self.connected_user = None
     def set_user(self,username,adress):
         self.username=username
         self.address=adress
-    
+    def connect(self,user):
+        self.connected_user=user
+
