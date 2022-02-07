916d33e653a8ff2bac8f55ea305b4dfb259e070d
barg991 <barg991@gmail.com>
2022-02-07 20:17:52 +0200

    made code cleaner

--- src/Client.py
+++ src/Client.py
@@ -1,17 +1,14 @@
 # import socket
 from socket import *
 
-#SERVER_ADDRESS = ('localhost', 13000)
 serverName = 'localhost'
-serverPort = 13000
+serverPort = 55001
 SERVER_ADDRESS = (serverName, serverPort)
 
-# clientSocket = socket.socket()
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(SERVER_ADDRESS)
 sentence = input('Input lowercase sentence:')
 
-# clientSocket.send(bytes(sentence, encoding="UTF-8"))
 clientSocket.send(sentence.encode())
 
 modifiedSentence = clientSocket.recv(4096)
--- src/Server.py
+++ src/Server.py
@@ -1,20 +1,18 @@
 #import socket
 from socket import *
 
-SERVER_ADDRESS = ('', 13000)
-# serverSocket = socket.socket()
+SERVER_ADDRESS = ('', 55001)
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 serverSocket.bind(SERVER_ADDRESS)
-serverSocket.listen(1)
+serverSocket.listen(5)
 print("The server is ready to receive client")
 while True:
     connectionSocket, addrClient = serverSocket.accept()
-    # sentence = connectionSocket.recv(4096)
     sentence = connectionSocket.recv(4096).decode()
 
     print("Get from client ",addrClient, ":", sentence)
     capitalizedSentence = sentence.upper()
     connectionSocket.send(bytes(capitalizedSentence.encode()))
-    # connectionSocket.send(capitalizedSentence).encode()
+    #connectionSocket.send(capitalizedSentence).encode()
     connectionSocket.close()
