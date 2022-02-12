a93e694789facec7fbf10d0b76ccc9b4fd675039
barg991 <barg991@gmail.com>
2022-02-12 18:05:12 +0200

    chat works between multiple clients v.3 (can select which client to talk to)

--- src/Server.py
+++ src/Server.py
@@ -4,13 +4,18 @@ import concurrent.futures
 from User import *
 
 '''
-    Left to Do:
-        - need to find a way to start connection (done for basic use)
-        - select who to send to
-        - 
+    Left to do:
+        - Send message to all users.
+        - GUI
+        - Finish all commands.
     How it works so far:
-        - when a client connects to the server he sends his name first that 
+        - When a client connects to the server he sends his name first that 
           gets appended to the connections dictionary including his connection socket.
+          Then he chooses who to talk to (he himself is default),
+          he sends a message to the server who sends the message through the socket to whom he wants
+          to communicate with.
+          Each client is a thread in the server (we used threadpools)
+          
 '''
 SERVER_ADDRESS = ('', 55016)
 serverSocket = socket(AF_INET, SOCK_STREAM)
