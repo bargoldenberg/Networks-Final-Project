d35bca8c112dffd7f5c32e03c3181810a50c6dfb
barg991 <barg991@gmail.com>
2022-02-20 16:34:04 +0200

    implementing RDT over UDP

--- src/Client.py
+++ src/Client.py
@@ -5,17 +5,68 @@ import time
 
 class Client:
     def __init__(self):
-        serverName = '192.168.14.17'
-        serverPort = 55017
+        serverName = '192.168.14.189'
+        serverPort = 55006
+        udpserverport = 55007
         self.SERVER_ADDRESS = (serverName, serverPort)
+        self.UDP_SERVER_ADRESS = (serverName,udpserverport)
         self.clientSocket = socket(AF_INET, SOCK_STREAM)
+        self.udpclientsocket = socket(AF_INET,SOCK_DGRAM)
         self.clientSocket.connect(self.SERVER_ADDRESS)
+        #self.udpclientsocket.connect(self.UDP_SERVER_ADRESS)
         self.username = None
 
     def init_connect(self, username):
         self.clientSocket.send(username.encode())
 
+    def checksum(self,data):
+        pos = len(data)
+        if (pos & 1):  # If odd...
+            pos -= 1
+            sum = ord(data[pos])  # Prime the sum with the odd end byte
+        else:
+            sum = 0
+
+        # Main code: loop to calculate the checksum
+        while pos > 0:
+            pos -= 2
+            sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+
+        sum = (sum >> 16) + (sum & 0xffff)
+        sum += (sum >> 16)
+
+        result = (~ sum) & 0xffff  # Keep lower 16 bits
+        result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+        return chr(result / 256) + chr(result % 256)
+
     def send_message(self, message):
+        if message == 'File.txt':
+            File = open('recvFile.txt', 'w')
+            print('one')
+            self.udpclientsocket.sendto('gimmie a file'.encode(),self.UDP_SERVER_ADRESS)
+            print('two')
+            while True:
+                data = self.udpclientsocket.recv(4096).decode()
+                print(data)
+                header = data.split(';')
+                seq = header[0]
+                checksum = header[1]
+                payload = ''
+                i=2
+                while i < len(header):
+                    if len(header) == 3 or i == len(header)-1:
+                        payload += header[i]
+                        i+=1
+                    else:
+                        payload +=header[i]+';'
+                        i+=1
+                print(payload)
+                # if checksum!=self.checksum(payload):
+                #     self.udpclientsocket.sendto(('NAK'+str(seq)).encode(),self.UDP_SERVER_ADRESS)
+                # else:
+                #     self.udpclientsocket.sendto(('ACK'+str(seq)).encode(), self.UDP_SERVER_ADRESS)
+                File.write(payload)
+            return
         if self.username is None:
             self.username = input('Input Username:')
             self.init_connect(self.username)
--- src/Server.py
+++ src/Server.py
@@ -2,7 +2,8 @@
 from socket import *
 import concurrent.futures
 from User import *
-
+from scapy import all
+import threading
 '''
     Left to do:
         - Send message to all users.
@@ -19,11 +20,14 @@ from User import *
 '''
 class Server():
     def __init__(self):
-        self.SERVER_ADDRESS = ('192.168.14.17', 55017)
+        self.SERVER_ADDRESS = ('192.168.14.189', 55006)
+        self.SERVER_ADDRESS_UDP = ('192.168.14.189', 55007)
         self.serverSocket = socket(AF_INET, SOCK_STREAM)
+        self.serverSocket_udp = socket(AF_INET,SOCK_DGRAM)
         self.connections = {}
         self.sock = {}
         self.serverSocket.bind(self.SERVER_ADDRESS)
+        self.serverSocket_udp.bind(self.SERVER_ADDRESS_UDP)
         self.serverSocket.listen(5)
         print("The server is ready to receive clients")
 
@@ -71,9 +75,67 @@ class Server():
                     connection_socket.send(bytes('Sent!\n'.encode()))
                     self.connections[addr_client].connected_user.send(bytes(sentencefrom.encode() + sentence.encode()))
 
+    def create_pkt(self,data,SEGMENTSIZE,offset,size):
+        if SEGMENTSIZE+offset>size:
+            return data[offset:]
+        else:
+            return data[offset:offset+SEGMENTSIZE]
+
+    def checksum(self,data):
+        pos = len(data)
+        if (pos & 1):  # If odd...
+            pos -= 1
+            sum = ord(data[pos])  # Prime the sum with the odd end byte
+        else:
+            sum = 0
+
+        # Main code: loop to calculate the checksum
+        while pos > 0:
+            pos -= 2
+            sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+
+        sum = (sum >> 16) + (sum & 0xffff)
+        sum += (sum >> 16)
+
+        result = (~ sum) & 0xffff  # Keep lower 16 bits
+        result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+        return chr(result / 256) + chr(result % 256)
+
+    def segment(self,data,size):
+        packets = []
+        SEGMENTSIZE = 5
+        OFFSET=0
+        seq = 0
+        while OFFSET<=size:
+            payload = self.create_pkt(data, SEGMENTSIZE, OFFSET, size)
+            check_sum = self.checksum(payload)
+            packets.append(f'{seq};{check_sum};{payload}')
+            seq+=1
+            OFFSET+=SEGMENTSIZE
+        return packets
+
+    def run_udp(self):
+        while True:
+            print('1')
+            message, clientaddress = self.serverSocket_udp.recvfrom(4096)
+            print('2')
+            file = open('/home/bar/PycharmProjects/Networks-Final-Project/Data/File.txt','r')
+            data = file.read()
+            packets = self.segment(data,len(data))
+            print(packets)
+            for packet in packets:
+                self.serverSocket_udp.sendto(packet.encode(),clientaddress)
+            print(data)
+    def listen(self):
+        while True:
+            print(self.serverSocket_udp.recv(4096).decode())
 
 # ThreadPool run 5 threads
 if __name__ == '__main__':
     server = Server()
     with concurrent.futures.ThreadPoolExecutor() as executor:
         clients = [executor.submit(server.run) for _ in range(5)]
+        udp_clients = [executor.submit(server.run_udp) for _ in range(5)]
+    # t = threading.Thread(target = server.listen,args =[])
+    # t.start
+    # t.join
